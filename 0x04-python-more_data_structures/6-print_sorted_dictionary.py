874bdb44ef7fc3f7a25f60c2870c4988c93f0c77
Jesse-Jagger <jessejackson.055@gmail.com>
2023-12-08 21:57:33 -0800

    updates

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    list_ord = list(a_dictionary.keys())
+    list_ord.sort()
+
+    for j in list_ord
+    print(f"{j}: {a_dictionary.keys()}")
