f82221270ac4100c2c6a01c86c0758a186053ffa
Jesse-Jagger <jessejackson.055@gmail.com>
2023-12-19 21:08:31 -0800

    updates

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module creates square class"""
+
+
+class Square:
+   
+   """This is a Class Square that defines a square.
+    """
+   
+   pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""This module creates square class"""
+
+
+class Square:
+
+    """Class Square that defines a square.
+    """
+    
+    def __init__(self, size):
+        
+        """Initialize the class"""
+
+        self.__size = size
