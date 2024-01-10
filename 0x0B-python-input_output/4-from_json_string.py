f3d6229717806eb7e5467336eb626e528b05c6d8
Jesse-Jagger <jessejackson.055@gmail.com>
2024-01-09 21:24:21 -0800

    updates

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+"""Defines a JSON-to-object function."""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the Python object representation of a JSON string."""
+    return json.loads(my_str)
