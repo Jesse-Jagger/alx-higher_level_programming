d34a0d2e7a96caf31f23d7b7c09853d7b3ad9f07
Jesse-Jagger <jessejackson.055@gmail.com>
2024-02-15 18:53:03 -0800

    updates

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- a script that wiil insert a new row in the table 'first_table' in your MySQL server
+
+INSERT INTO first_table(id, name) VALUES(89, 'Best School');
+
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- a script that will display the number of records with 'id = 89' in the table 'first_table' of the databases in your MySQL server
+
+SELECT COUNT(*) FROM first_table WHERE id=89;
