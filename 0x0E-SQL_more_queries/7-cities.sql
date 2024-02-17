dc117e0a6d50c20404a26f4b6322f41ca966c449
Jesse-Jagger <jessejackson.055@gmail.com>
2024-02-17 09:13:19 -0800

    updates

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,6 +5,5 @@ CREATE DATABASE hbtn_0d_usa;
 CREATE TABLE hbtn_0d_usa.cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
-);
+	FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
 
