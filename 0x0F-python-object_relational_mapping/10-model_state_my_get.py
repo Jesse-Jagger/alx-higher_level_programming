e7c0b8e87a3a992f96a43f409bbfa5894465240b
Jesse-Jagger <jessejackson.055@gmail.com>
2024-03-18 22:12:50 -0700

    updates

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-""" that prints the first State object from the database hbtn_0e_6_usa
-"""
+
+""" that prints the State object with the name passed as argument from the database"""
 import sys
 from model_state import Base, State
 from sqlalchemy import (create_engine)
@@ -9,9 +9,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[3], sys.argv[5]))
+                           .format(sys.argv[3], sys.argv[2], sys.argv[1]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).filter(State.name.like('%a%')):
-        print(instance.id, instance.name, sep=": ")
+    instance = session.query(State).filter(State.name == (sys.argv[4],))
+    try:
+        print(instance[0].id)
+    except IndexError:
+        print("Not found")
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-"""that Contains the class definition of a City"""
-
-from relationship_state import Base
+""" that Contains the class definition of a City"""
+from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
 
 class City(Base):
     """A Class that defines each city"""
-
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
