710ff5bd94ddae70849d28aaf6457543787b853f
Your Name <jessejackson.055@gmail.com>
2024-03-27 21:56:37 -0700

    updates

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 # Sends a DELETE request to a provided URL and displays the response body.
-
 curl -sX DELETE "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-
 # Displays all HTTP methods of the server for a provided URL will accept.
-
 curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
