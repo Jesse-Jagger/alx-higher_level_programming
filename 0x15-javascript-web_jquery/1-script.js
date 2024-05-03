8614203f791bdde4292b063873a65c1affa3497f
jesse-jagger <jessejackson.055@gmail.com>
2024-05-02 22:15:04 -0700

    updates

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,3 @@
+// Uses the jQuery API to change color of header tag to red
+
+$('header').css('color', '#FF0000');
---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,6 @@
+// Toggles the class of HTML tag "HEADER" when a user clicks the
+// div#toggle_header tag.
+
+$('div#toggle_header').click(function () {
+    $('header').ToggleClass('red');
+  });
